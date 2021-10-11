240452330858dc166f7a1f9a4e707cb258ed3f0a
ubirajaraalmeida <biraneto.j@gmail.com>
2021-10-11 15:44:08 -0300

    feat(header): header pardão

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <Sidebar />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Header from './components/layout/Header.vue'
+import Sidebar from './components/layout/Sidebar.vue'
+import Footer from './components/layout/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Sidebar,
+    Footer
   }
 }
 </script>
+
 <style>
-body { 
+body {
+  display: block;
   background-color: #f1f1f1;
-  color: #fff;  
+  color: #fff; 
+  border: solid 1px red;
+  height: 100%;
 }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
-        
+        <header>
+            <div class="container-fluid">
+                <div>
+                    <a href="/">
+                        <img :src="logo" :alt="descricao" />
+                    </a>
+                </div>
+                <div>
+                    <div class="btn-group">
+                        <button type="button" class="dropdown-toggle menuUsuario" data-bs-toggle="dropdown" aria-expanded="false">
+                            <div class="circuloImagem">
+                                <img :src="imagemUsuario" :alt="descricaoImagemUsuario" class="imagemUsuario" />
+                            </div>
+                            <span>{{ nomeUsuario }}</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><button class="dropdown-item" type="button">Perfil</button></li>
+                            <li><button class="dropdown-item" type="button">Configurações</button></li>
+                            <li><button class="dropdown-item" type="button">Sair</button></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>   
+        </header>
     </div>
 </template>
 
@@ -9,8 +32,63 @@ export default {
     name: 'Header',
     data(){
         return{
-
+            logo: "/img/logo.png",
+            descricao: "Logo",
+            imagemUsuario: "/img/ubirajara.png",
+            nomeUsuario: "Ubirajara Jucá",
+            descricaoImagemUsuario: "Imagem do usuário",
         }
-    }
+    },
 }
 </script>
+<style scoped>
+header{
+    position: fixed;
+    width: 100%;
+    height: 60px;
+    background-color: blue;
+    top: 0;
+    left: 0;
+}
+header .container-fluid{
+    display: flex;
+    justify-content: space-between;
+}
+header a img {
+    width: 50px;
+    margin: 5px;
+}
+header nav{
+    width: 100%;
+}
+header button.menuUsuario {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    width: auto;
+    height: 60px;
+    border: 0;
+    background-color: transparent;
+    color: white;
+    padding: 0 10px;
+}
+header button.menuUsuario:hover {
+    background-color:rgba(0, 0, 0, 0.1);
+}
+header button.menuUsuario .circuloImagem {
+    border-radius: 50%;
+    overflow: hidden;
+    margin: 10px 0;
+}
+header button.menuUsuario img.imagemUsuario {
+    width: 40px;
+    height: 40px;
+}
+header button.menuUsuario span{
+    font-weight: bold;
+    display: block;
+    height: 60px;
+    line-height: 60px;
+    padding: 0 5px;
+}
+</style>
